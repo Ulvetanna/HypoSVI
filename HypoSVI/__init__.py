d1c275a42e0d2a856edc273c22f4cc99ea975b99
Ulvetanna <jds70@cam.ac.uk>
2020-09-18 11:19:54 -0700

    Initialisation - Corrections to __init__.py

--- HypoSVI/__init__.py
+++ HypoSVI/__init__.py
@@ -1,4 +1,4 @@
-name = "EikoNet"
+name = "HypoSVI"
 __version__ = "1.0"
 __description__ = "HypoSVI: Earthquake location using Stein-variation gradient descent and deep neural network travel-time tables"
 __license__ = "GPL v3.0"
