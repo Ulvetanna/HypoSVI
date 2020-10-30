15884ade148d5e1ba895c7d7cd4628787a6b336a
Ulvetanna <jds70@cam.ac.uk>
2020-10-30 10:35:22 -0700

    Updating HypoSVI to use latest EikoNet

--- build/lib/HypoSVI/__init__.py
+++
@@ -1,6 +0,0 @@
-name = "HypoSVI"
-__version__ = "1.0"
-__description__ = "HypoSVI: Earthquake location using Stein-variation gradient descent and deep neural network travel-time tables"
-__license__ = "GPL v3.0"
-__author__ = "Jonathan Smith, Kamyar Azizzadenesheli, Zachary E. Ross, Jack Muir"
-__email__ = "jon_smith83@hotmail.co.uk"
